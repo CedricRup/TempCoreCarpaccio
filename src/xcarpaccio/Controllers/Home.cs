a92811d5eecf9e275038b5a9c26ac3e5d2099651
Cedric Rup <cedric.rup@gmail.com>
2016-12-06 13:45:01 +0100

    clean for publishing

--- src/xcarpaccio/Controllers/Home.cs
+++ src/xcarpaccio/Controllers/Home.cs
@@ -1,8 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using XCarpaccio.Models;
 
 namespace XCarpaccio.Controllers
 {
@@ -14,7 +10,6 @@ namespace XCarpaccio.Controllers
        public IActionResult Get()
        {
            return Content("Hello world of Carpaccio!");
-       }
-            
+       }       
     }
 }
